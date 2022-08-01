3b1c04ca30b4b3681fe288ea616527acfcd1696e
Flagada001 <francous000@gmail.com>
2022-07-31 21:29:03 -0400

    mouse look at, camera rotation

---
+++ Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour
+{
+    public GameObject character;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Camera Follow the player
+        transform.position = character.transform.position;
+
+        //Can move the camera if Right mouse is pressed
+        if (Input.GetAxis("Fire2") > 0)
+        {
+            //Move Camera Up and down
+            transform.Rotate(new Vector3(1, 0, 0) * Input.GetAxis("Mouse Y"));
+
+            //Move Camera left and Right
+            transform.Rotate(new Vector3(0, 1, 0) * Input.GetAxis("Mouse X"));
+
+            //Force camera rotation to be 0 on Z axis
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, 0);
+        }
+    }
+}
--- Assets/Scripts/MainControls.cs
+++ Assets/Scripts/MainControls.cs
@@ -4,28 +4,37 @@ using UnityEngine;
 
 public class MainControls : MonoBehaviour
 {
-    private float XMoveSpeed = 4;
-    private float XMoveInput;
-    private float ZMoveSpeed = 4;
-    private float ZMoveInput;
+    public float xMoveSpeed = 4;
+    private float xMoveInput;
+    public float zMoveSpeed = 4;
+    private float zMoveInput;
+    public float rotateSpeed = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Input.mousePosition);
-        transform.LookAt(Input.mousePosition);
+        // Debug.Log(Input.GetAxis("Mouse X"));
+        // transform.Rotate(new Vector3(0, 1, 0) * Input.GetAxis("Mouse X"));
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit);
+        Vector3 targetDirection = hit.point - transform.position;
+        float singleStep = rotateSpeed * Time.deltaTime;
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+        newDirection -= new Vector3(0, newDirection.y, 0);
+        transform.rotation = Quaternion.LookRotation(newDirection);
 
         // Move the Character on a flat plane
-        ZMoveInput = Input.GetAxis("Horizontal");
-        XMoveInput = Input.GetAxis("Vertical");
-        transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime * XMoveSpeed * XMoveInput);
-        transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * ZMoveSpeed * ZMoveInput);
+        xMoveInput = Input.GetAxis("Horizontal");
+        zMoveInput = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime * xMoveSpeed * xMoveInput);
+        transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * zMoveSpeed * zMoveInput);
 
     }
 }
