598dd85d65ea21b00eb4aca28ff63990f1f56e97
Flagada001 <francous000@gmail.com>
2022-08-07 19:37:53 -0400

    Limit enemy to 5

--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -20,10 +20,15 @@ public class SpawnEnemy : MonoBehaviour
     void Update()
     {
         countSecondToSpawn += Time.deltaTime;
+
+        // Limit enemy to 5
+        if (GameObject.FindGameObjectsWithTag("Enemy").Length >= 5) { return; }
+
         if (countSecondToSpawn > 3)
         {
             countSecondToSpawn = 0;
             enemyObject = Instantiate(EnemyPrefab, spawnPoint, Quaternion.identity);
+
         }
     }
 }
